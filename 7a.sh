d320e82712d88189cecd3caefa7b154741edb14a
Gururaj-Gg <79778518+Gururaj-Gg@users.noreply.github.com>
2022-03-11 14:35:49 +0530

    Update 7a.sh

--- 7a.sh
+++ 7a.sh
@@ -1,19 +1,29 @@
 
+
 set -- `who`
-g=$1
 t=$4
+u=`whoami`
+for i in $*
+do
+if [ $u != $1 ]
+then
+ `shift 5`
+else
+t=$4
+fi
+done
 set -- `echo $t | tr ":" " "`
 h=$1
 echo " user logged in terminal at: $h"
 if [ $h -ge 4 ] && [ $h -lt 12 ]
 then
-echo "GOOD MORNING mr. $g"
+echo "GOOD MORNING mr. $u"
 elif [ $h -ge 12 ]  && [ $h -lt 16 ]
 then
-echo "GOOD afternoon mr.$g"
+echo "GOOD afternoon mr.$u"
 elif [ $h -ge 16  ] && [ $h -lt 19 ]
 then
-echo "GOOD evening mr. $g"
+echo "GOOD evening mr. $u"
 else
-echo "GOOD night mr. $g"
+echo "GOOD night mr. $u"
 fi
