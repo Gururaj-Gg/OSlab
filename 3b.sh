78dfd2dc6a521d03717641fceaf6e6e4e53ab946
Gururaj-Gg <79778518+Gururaj-Gg@users.noreply.github.com>
2022-04-19 14:31:16 +0530

    Add files via upload

---
+++ 3b.sh
@@ -0,0 +1,34 @@
+echo "enter the password"
+stty -echo
+read p1
+stty -echo
+echo "confirm password"
+stty -echo
+read p2
+stty -echo
+while [ "$p1" != "$p2" ]
+do
+echo "re enter the correct password "
+stty -echo
+read p2
+stty -echo
+done
+clear
+echo "-------------------------Terminal Locked-----------------------------"
+echo " Enter the password to unlock"
+stty -echo
+read p3
+stty -echo
+while [ "$p1" != "$p3" ]
+do
+clear
+echo "--------------------------Terminal Locked-----------------------------"
+echo "password does not match"
+echo "enter the correct password"
+stty -echo
+read p3
+stty echo
+done
+clear
+echo "--------------------------Terminal Unlocked---------------------------"
+clear
